130574668ad6c566a241432d0c8778c34f7a0e74
Cristian Vlaicu <cristian_fly90@yahoo.es>
2024-10-24 19:19:23 +0200

    Main code formatted with BLACK. All comments on English. Generated new pdf document to show.

--- Text_files_to_PDF.py
+++ Text_files_to_PDF.py
@@ -1,4 +1,5 @@
-    # Vamos a crear un programa que dado unos texto en formato txt los vamoos a agrupar en un documento pdf con una página para cada documento de texto.
+# We are going to create a program that, given some text in txt format, will group them into a pdf document with one page for each text document.
+
 from fpdf import FPDF
 import glob
 from pathlib import Path
@@ -8,32 +9,34 @@ C = 0  # color.
 # Create a list of text filepaths
 filepaths = glob.glob(r"files\*.txt")
 
-# Creamos instancia de pdf acudiendo al módulo FPDF. Y le damos formato: orientación Portrait, unidades en mm y formato A4.
+# Create a pdf instance using the FPDF module. And we format it: Portrait orientation, units in mm and A4 format.
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-# Recorremos cada archivo de texto:
+# Iterate over each text file:
 for filepath in filepaths:
-    # Añadimos una página PDf por cada archivo de texto.
+    # Add a PDF page for each text file.
     pdf.add_page()
 
-    # Extraemos el noche de cada archivo de texto sin la extensión.
+    # Extract the name of each text file without the extension.
     filename = Path(filepath).stem
     document_name = filename.upper()
 
-    # Ponemos ese nombre a cada página del archivo PDf.
+    # Put that name on each page of the PDF file.
     pdf.set_font(family="Times", size=24, style="B")
     pdf.set_text_color(254, 0, 0)
-    pdf.cell(w=0, h=12, txt=document_name, align="C", ln = 1)
+    pdf.cell(w=0, h=12, txt=document_name, align="C", ln=1)
     pdf.line(86, 21, 124, 21)
     pdf.set_draw_color(C, C, C)
 
     pdf.cell(w=0, h=12, ln=2)
 
-    # Extraemos el contenido de cada archivo de texto abriéndolos...
+    # Extract the content of each text file by opening them...
     with open(filepath, "r") as file:
-        document_content = file.read()  # y guardando en esa variable el contenido de cada texto.
-    # Add the text file content to the PDf
-    pdf.set_font(family="Times", style= "I", size=12)
+        document_content = (
+            file.read()
+        )  # and saving the content of each text in that variable.
+    # Add the text file content to the PDF
+    pdf.set_font(family="Times", style="I", size=12)
     pdf.set_text_color(C, C, C)
     pdf.multi_cell(w=0, h=6, txt=document_content, align="J")
 
@@ -41,5 +44,6 @@ for filepath in filepaths:
     pdf.set_font(family="Times", style="B", size=10)
     pdf.set_text_color(C, C, C)
     pdf.cell(w=0, h=0, txt=str(pdf.page_no()), align="C")
+
 # Produce the PDF
 pdf.output("Text_files_to_PDF.pdf")
